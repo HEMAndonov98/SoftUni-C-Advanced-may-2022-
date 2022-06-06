1a33319684209ff5420b1c76b54c75462b813df9
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-06 09:57:46 +0300

    all lab exercises

---
+++ Streams-Files-and-Dictionaries/7.Folder-Size/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"../../../Files/TestFolder";
+            string outputPath = @"../../../Files/output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+            FileInfo[] files = dirInfo.GetFiles("*", SearchOption.AllDirectories);
+            double fileSize = 0;
+
+            foreach (var file in files)
+            {
+                fileSize += file.Length;
+            }
+
+            fileSize /= 1024;
+
+            File.WriteAllText(outputFilePath, fileSize.ToString());
+        }
+    }
+}
