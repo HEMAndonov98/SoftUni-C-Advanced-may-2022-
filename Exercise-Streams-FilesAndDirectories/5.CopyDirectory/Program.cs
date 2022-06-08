e7dca07d9d059c78313a75080a9b99b4346067c0
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-08 11:07:22 +0300

    exercises

---
+++ Exercise-Streams-FilesAndDirectories/5.CopyDirectory/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            //this is the information of the dir we're going to copy
+            var dir = new DirectoryInfo(inputPath);
+            if(!dir.Exists)
+                Console.WriteLine("Directory doesn't exist");
+
+            //this is an array containing all the files inside the directory
+            var files = dir.GetFiles();
+
+            string targetDirectoryPath = Path.Combine(outputPath, dir.Name);
+
+            //if directory exists delete it so we don't get an exeption
+            if (Directory.Exists($"{targetDirectoryPath}-copy"))
+                Directory.Delete($"{targetDirectoryPath}-copy", true);
+            //We create a dir with the same name as the original dir but we add a "-copy" at the end
+            Directory.CreateDirectory($"{targetDirectoryPath}-copy");
+
+            foreach (var file in files)
+            {
+                //each file is given a new path consisting of the new directory path and the files original name
+                string targetFilePath = Path.Combine($"{targetDirectoryPath}-copy", file.Name);
+                file.CopyTo(targetFilePath);
+            }
+        }
+    }
+}
---
+++ Exercise-Streams-FilesAndDirectories/6.Zip-and-Extracts/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"../../../copyMe.png";
+            string zipArchiveFile = @"../../../archive.zip";
+            string extractedFile = @"../../../extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var source = new FileStream(inputFilePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (var archive = new ZipArchive(source, ZipArchiveMode.Create))
+                {
+                    archive.CreateEntry(source.Name);
+                }
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
