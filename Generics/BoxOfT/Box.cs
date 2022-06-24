e31c5fdf07639a0a6f035af7ef56931127392127
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-24 22:27:07 +0300

    Generics Lab

---
+++ Generics/BoxOfT/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+	public class Box<T>
+	{
+		private List<T> data;
+		private int count => this.data.Count;
+
+		public Box()
+		{
+			this.data = new List<T>();
+		}
+
+		public int Count { get { return count; } }
+
+		public void Add(T element)
+        {
+			this.data.Add(element);
+        }
+		public T Remove()
+        {
+            if (this.Count == 0)
+				throw new ArgumentOutOfRangeException();
+
+			var element = this.data.Last();
+			this.data.RemoveAt(this.Count - 1);
+			return element;
+        }
+	}
+}
+
---
+++ Generics/GenericScale/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var equality = new EqualityScale<int>(2, 2);
+            Console.WriteLine(equality.AreEqual());
+        }
+    }
+}
+
