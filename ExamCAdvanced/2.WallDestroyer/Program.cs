93c869d5d13704cba407fe11a31326e6c36a867b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-25 13:29:45 +0300

    Exam c# advanced

---
+++ ExamCAdvanced/2.WallDestroyer/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.WallDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+            int rowPosition = 0;
+            int colPosition = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+                char[] layout = FillLayout(input);
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = layout[col];
+                    if (matrix[row, col] == 'V')
+                    {
+                        rowPosition = row;
+                        colPosition = col;
+                        matrix[row, col] = '*';
+                    }
+                }
+            }
+
+            int rodsHit = 0;
+            int holesMade = 1;
+
+            Tuple<int, int> up = new Tuple<int, int>(-1, 0);
+            Tuple<int, int> down = new Tuple<int, int>(1, 0);
+            Tuple<int, int> left = new Tuple<int, int>(0, -1);
+            Tuple<int, int> right = new Tuple<int, int>(0, 1);
+
+            string command;
+            while ((command = Console.ReadLine()) != "End" )
+            {
+                if (command == "up")
+                {
+                    if (!CheckBoundaries(rowPosition, colPosition, up, matrix))
+                        continue;
+                    rowPosition += up.Item1;
+                    colPosition += up.Item2;
+                }
+                else if (command == "down")
+                {
+                    if (!CheckBoundaries(rowPosition, colPosition, down, matrix))
+                        continue;
+                    rowPosition += down.Item1;
+                    colPosition += down.Item2;
+                }
+                else if (command == "left")
+                {
+                    if (!CheckBoundaries(rowPosition, colPosition, left, matrix))
+                        continue;
+                    rowPosition += left.Item1;
+                    colPosition += left.Item2;
+                }
+                else if (command == "right")
+                {
+                    if (!CheckBoundaries(rowPosition, colPosition, right, matrix))
+                        continue;
+                    rowPosition += right.Item1;
+                    colPosition += right.Item2;
+                }
+
+                if (matrix[rowPosition, colPosition] == '-')
+                {
+                    matrix[rowPosition, colPosition] = '*';
+                    holesMade++;
+                }
+                else if (matrix[rowPosition, colPosition] == '*')
+                {
+                    Console.WriteLine($"The wall is already destroyed at position [{rowPosition}, {colPosition}]!");
+                }
+                else if (matrix[rowPosition, colPosition] == 'R')
+                {
+                    Console.WriteLine("Vanko hit a rod!");
+                    rodsHit++;
+                    if (command == "up")
+                    {
+
+                        rowPosition -= up.Item1;
+                        colPosition -= up.Item2;
+                    }
+                    else if (command == "down")
+                    {
+
+                        rowPosition -= down.Item1;
+                        colPosition -= down.Item2;
+                    }
+                    else if (command == "left")
+                    {
+
+                        rowPosition -= left.Item1;
+                        colPosition -= left.Item2;
+                    }
+                    else if (command == "right")
+                    {
+
+                        rowPosition -= right.Item1;
+                        colPosition -= right.Item2;
+                    }
+                }
+                else if (matrix[rowPosition, colPosition] == 'C')
+                {
+                    matrix[rowPosition, colPosition] = 'E';
+                    holesMade++;
+                    Console.WriteLine($"Vanko got electrocuted, but he managed to make {holesMade} hole(s).");
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            Console.Write(matrix[row, col]);
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                }
+            }
+            if (command == "End")
+            {
+                matrix[rowPosition, colPosition] = 'V';
+                Console.WriteLine($"Vanko managed to make {holesMade} hole(s) and he hit only {rodsHit} rod(s).");
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write(matrix[row, col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static char[] FillLayout(string input)
+        {
+            char[] temp = new char[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                temp[i] = input[i];
+            }
+            return temp;
+        }
+        public static bool CheckBoundaries(int row, int col, Tuple<int, int> direction, char[,] matrix)
+        {
+            row += direction.Item1;
+            col += direction.Item2;
+
+            if (row >= 0 && row < matrix.GetLength(0) &&
+                col >= 0 && col < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
+
---
+++ ExamCAdvanced/TileMaster/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TileMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var locations = new Dictionary<string, int>()
+            {
+                {"Sink", 40 },
+                {"Oven", 50 },
+                {"Countertop", 60 },
+                {"Wall", 70 },
+            };
+            var fittedTiles = new Dictionary<string, int>
+            {
+                {"Sink", 0 },
+                {"Oven", 0 },
+                {"Countertop", 0 },
+                {"Wall", 0 },
+                {"Floor", 0 }
+            };
+            var whiteTiles = new Stack<int>();
+            var greyTiles = new Queue<int>();
+
+            int[] whiteTileAreas = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] greyTileAreas = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            FillWhiteStack(whiteTiles, whiteTileAreas);
+            FillGreyTiles(greyTiles, greyTileAreas);
+
+
+            string tilesState = string.Empty;
+            while ((tilesState = CheckTilesCount(greyTiles, whiteTiles)) == "has tiles")
+            {
+                var firstGreyTile = greyTiles.Peek();
+                var lastWhiteTile = whiteTiles.Peek();
+                bool hasFittedATile = false;
+
+                if (firstGreyTile != lastWhiteTile)
+                {
+                    greyTiles.Enqueue(greyTiles.Dequeue());
+                    whiteTiles.Pop();
+                    whiteTiles.Push(lastWhiteTile / 2);
+                    continue;
+                }
+
+                var newTile = lastWhiteTile + firstGreyTile;
+
+                foreach (var (location, areaRequired) in locations)
+                {
+                    if (newTile == areaRequired)
+                    {
+                        greyTiles.Dequeue();
+                        whiteTiles.Pop();
+                        fittedTiles[location]++;
+                        hasFittedATile = true;
+                        break;
+                    }
+                }
+                if (hasFittedATile)
+                    continue;
+
+                fittedTiles["Floor"]++;
+                greyTiles.Dequeue();
+                whiteTiles.Pop();
+
+            }
+
+            PrintTiles(greyTiles, whiteTiles, tilesState);
+            PrintLocations(fittedTiles);
+
+        }
+
+        public static void FillWhiteStack(Stack<int> whiteTiles, int[] whiteTileAreas)
+        {
+            for (int i = 0; i < whiteTileAreas.Length; i++)
+            {
+                whiteTiles.Push(whiteTileAreas[i]);
+            }
+        }
+        public static void FillGreyTiles(Queue<int> greyTiles, int[] greyTileAreas)
+        {
+            for (int i = 0; i < greyTileAreas.Length; i++)
+            {
+                greyTiles.Enqueue(greyTileAreas[i]);
+            }
+        }
+        public static string CheckTilesCount(Queue<int> greyTiles, Stack<int> WhiteTiles) 
+        {
+            if (greyTiles.Count == 0 && WhiteTiles.Count == 0)
+            {
+                return "both";
+            }
+            else if (greyTiles.Count == 0)
+            {
+                return "grey";
+            }
+            else if(WhiteTiles.Count == 0)
+            {
+                return "white";
+            }
+
+            return "has tiles";
+        }
+        public static void PrintTiles(Queue<int> greyTiles, Stack<int> WhiteTiles, string tileState)
+        {
+            if (tileState == "both")
+            {
+                Console.WriteLine("White tiles left: none");
+                Console.WriteLine("Grey tiles left: none");
+            }
+            else if (tileState == "grey")
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ", WhiteTiles)}");
+                Console.WriteLine("Grey tiles left: none");
+            }
+            else if (tileState == "white")
+            {
+                Console.WriteLine("White tiles left: none");
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", greyTiles)}");
+            }
+        }
+        public static void PrintLocations(Dictionary<string, int> fittedTiles)
+        {
+            fittedTiles = fittedTiles.OrderByDescending(t => t.Value)
+                .ThenBy(t => t.Key)
+                .ToDictionary(l => l.Key, v => v.Value);
+            foreach (var (location, tilesCount) in fittedTiles)
+            {
+                if(tilesCount > 0)
+                Console.WriteLine($"{location}: {tilesCount}");
+            }
+        }
+    }
+}
+
