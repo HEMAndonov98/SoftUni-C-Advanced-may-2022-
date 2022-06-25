ba1fc1b9b9c4e764caeaf10d607ef40f2fa1cd1e
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-25 13:34:01 +0300

    Renovators

---
+++ ExamCAdvanced/3.Renovators/Catalog.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.Renovators = new List<Renovator>();
+        }
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public int Count { get; private set; }
+        public List<Renovator> Renovators { get; set; }
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return null;
+            }
+            if (this.Count == this.NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+            Renovators.Add(renovator);
+            this.Count++;
+            return $"Successfully added {renovator.Name} to the catalog.";
+
+        }
+        public bool RemoveRenovator(string name)
+        {
+            for (int i = 0; i < this.Renovators.Count; i++)
+            {
+                if (this.Renovators[i].Name == name)
+                {
+                    this.Renovators.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            int numberOfRemovedRenovators = 0;
+
+            for (int i = 0; i < Renovators.Count; i++)
+            {
+                if (this.Renovators[i].Type == type)
+                {
+                    this.Renovators.RemoveAt(i);
+                    numberOfRemovedRenovators++;
+                }
+            }
+            return numberOfRemovedRenovators;
+        }
+        public Renovator HireRenovator(string name)
+        {
+            var hiredRenovator = this.Renovators.FirstOrDefault(r => r.Name == name);
+            hiredRenovator.Hire();
+            return hiredRenovator;
+        }
+        public List<Renovator> PayRenovators(int days)
+        {
+            return  this.Renovators.Where(r => r.Days >= days).ToList();
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Renovators available for Project {this.Project}:");
+            for (int i = 0; i < this.Renovators.Count; i++)
+            {
+                if (this.Renovators[i].Hired == false)
+                {
+                    sb.AppendLine(this.Renovators[i].ToString());
+                }
+            }
+            return sb.ToString();
+        }
+
+    }
+
+}
---
+++ ExamCAdvanced/3.Renovators/Renovator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+        }
+        
+
+        private string _name;
+        private string _type;
+        private double _rate;
+        private int _days;
+        private bool _hired = false;
+
+        public string Name
+        {
+            get { return _name; }
+            set { this._name = value; }
+        }
+        public string Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+        public double Rate
+        {
+            get { return this._rate; }
+            set { this._rate = value; }
+        }
+        public int Days
+        {
+            get { return this._days; }
+            set { this._days = value; }
+        }
+        public bool Hired { get { return _hired; }}
+
+        public void Hire()
+        {
+            this._hired = true;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"-Renovator: {this._name}");
+            sb.AppendLine($"--Specialty: {this._type}");
+            sb.Append($"--Rate per day: {this._rate} BGN");
+            return sb.ToString();
+        }
+    }
+}
