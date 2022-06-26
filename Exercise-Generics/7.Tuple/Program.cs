6b3612539a10f346d5112153177b6e0270c8916b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-06-26 15:17:50 +0300

    Exercise 7 and 8

---
+++ Exercise-Generics/7.Tuple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _7.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameAndAddress = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var fullName = $"{nameAndAddress[0]} {nameAndAddress[1]}";
+            var address = nameAndAddress[2];
+
+            var nameAddress = new MyTuple<string, string>(fullName, address);
+
+            var nameAndBeer = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var name = nameAndBeer[0];
+            var liters = int.Parse(nameAndBeer[1]);
+
+            var nameBeer = new MyTuple<string, int>(name, liters);
+
+            var numbersInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var intNumber = int.Parse(numbersInput[0]);
+            var doubleNumber = double.Parse(numbersInput[1]);
+
+            var numbers = new MyTuple<int, double>(intNumber, doubleNumber);
+
+            Console.WriteLine(nameAddress);
+            Console.WriteLine(nameBeer);
+            Console.WriteLine(numbers);
+
+            
+        }
+    }
+}
+
---
+++ Exercise-Generics/8.Threeuple/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _8.Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input 1
+            //{first name} {last name} {address} {town} all are of type string
+
+            var personInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var fullName = $"{personInfo[0]} {personInfo[1]}";
+            var address = personInfo[2];
+            var town = personInfo[3];
+
+            var personThreeple = new Threeuple<string, string, string>(fullName, address, town);
+
+            //Input 2
+            //string{name} int{liters of beer} bool{drunk or not}
+
+            var beerInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var name = beerInfo[0];
+            var liters = int.Parse(beerInfo[1]);
+            var isDrunk = beerInfo[2] == "drunk" ? true : false;
+
+            var beerThreeple = new Threeuple<string, int, bool>(name, liters, isDrunk);
+
+            //Input 3
+            //string{name} double{account balance} string{bank name}
+
+            var accountInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var accountHolder = accountInfo[0];
+            var accountBalance = double.Parse(accountInfo[1]);
+            var bankName = accountInfo[2];
+
+            var accountThreeple = new Threeuple<string, double, string>(accountHolder, accountBalance, bankName);
+
+            //Print all Threeple's
+            Console.WriteLine(personThreeple);
+            Console.WriteLine(beerThreeple);
+            Console.WriteLine(accountThreeple);
+
+            //Example Input :
+
+            //Adam Smith Wallstreet New York
+            //Mark 18 drunk
+            //Karren 0.10 USBank
+        }
+    }
+}
+
